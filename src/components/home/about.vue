6338aca83d87fd11246f9e2b04016bd263b59208
stivo-m <maichstivo254@gmail.com>
2020-07-05 14:53:27 +0300

    minor change

--- src/components/home/about.vue
+++ src/components/home/about.vue
@@ -6,7 +6,7 @@
         <div class="col-lg-8 text-center">
           <h2 class="text-white mt-0">We've got what you need!</h2>
           <hr class="divider light my-4" />
-          <p class="text-white-50 mb-4">
+          <p class="text-white-60 mb-4">
             Do you feel juggling work, studying, exams, and a personal life can
             sometimes feel a little bit overwhelming? You’re not alone, a lot of
             students do. College life these days is demanding and requires a lot
@@ -16,7 +16,7 @@
           </p>
 
           <p
-            class="text-white-50"
+            class="text-white-60"
           >Curtsy Writing is affordable for 98% of students around the globe. We kno that students struggle with both finances and their assignments, which is why we provide the best quality at an affordable rate. Therefore, we are always offering an affordable essay service price for our clients.</p>
           <router-link class="btn btn-light btn-xl js-scroll-trigger" to="/customer">Get Started!</router-link>
         </div>
